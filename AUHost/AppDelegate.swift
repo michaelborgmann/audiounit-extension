bc7a41566b5410df41cee09f315cbc052cba01e9
Michael Borgmann <michaelborgmann@mailbox.org>
2020-01-08 22:52:26 +0000

    Initial Commit

---
+++ AUHost/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AUHost
+//
+//  Created by Michael Borgmann on 08.01.20.
+//  Copyright © 2020 Michael Borgmann. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
